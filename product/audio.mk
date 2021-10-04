e3e2049450825f851acb5d9f500e8b1cdc00bb8d
TARKZiM <tom8476oo@gmail.com>
2021-10-04 20:48:54 +0800

    yuga: audio: Move configs to vendor

--- product/audio.mk
+++ product/audio.mk
@@ -1,4 +1,4 @@
 # Audio configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
